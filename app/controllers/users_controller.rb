5cba6d8dd3b1c83d0bd375b0800335cbd3360dcd
johnwojo <jwojo13@gmail.com>
2018-10-19 13:46:51 -0400

    Moved the code from users helper to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @daily_teacher
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,2 @@
 module UsersHelper
-  before_action :authenticate_user!
-
-  def show
-    @daily_teacher
-  end
 end
