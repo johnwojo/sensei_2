09bde9cedd38ef034dd758e017307bb3b1da8be6
johnwojo <jwojo13@gmail.com>
2018-10-24 14:30:36 -0400

    Added models for keywords, links, teachers, teachings, and teaching_keywords.

--- app/models/teaching.rb
+++ app/models/teaching.rb
@@ -1,2 +1,14 @@
 class Teaching < ApplicationRecord
+  belongs_to :teacher
+  has_many :teaching_keywords
+  has_many :keywords, :through => :teaching_keywords
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :media_type, presence: true
+  validates :favorite, presence: true
+  validates :teacher_id, presence: true
+
+  def daily_teacher
+    @daily_teacher = current_user.teachings.sample
+  end
 end
--- app/models/teaching_keyword.rb
+++ app/models/teaching_keyword.rb
@@ -1,2 +1,4 @@
 class TeachingKeyword < ApplicationRecord
+  belongs_to :teaching
+  belongs_to :keyword
 end
