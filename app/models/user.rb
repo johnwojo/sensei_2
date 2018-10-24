2d7870786ce0a22b6ffc400a2dddcc52340972ad
johnwojo <jwojo13@gmail.com>
2018-10-24 14:28:49 -0400

    Added user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ApplicationRecord
+  has_many :teachers
+  has_many :links
+  has_many :teachings, through: :teachers
+  validates :email, presence: true
+  validates :password, length: { in: 6..20 }
+  validates :email, uniqueness: true
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+
+  def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.name = auth.info.name
+       user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]
+     end
+   end
+
+   def self.most_common_teacher(user)
+     user.teachers.sort_by {|teacher| teacher.teachings.count}.last.name
+   end
 end
